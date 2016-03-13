7070ca6343ebc1a47bfa1353359328abd186cb90
Eric Freeman <efreeman@1800contacts.com>
2016-03-12 19:51:59 -0700

    Basic player movement.

---
+++ src/Assets/Scripts/Extensions/FloatExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Assets.Scripts.Extensions
+{
+    public static class FloatExtensions
+    {
+        public static float Clamp(this float source, float min, float max)
+        {
+            if (source < min) return min;
+            if (source > max) return max;
+            return source;
+        }
+
+        public static float MoveTowards(this float source, float destination, float speed)
+        {
+            if (Math.Abs(source) < Math.Abs(speed))
+            {
+                return 0;
+            }
+
+            if (source > 0)
+            {
+                return source - speed;
+            }
+
+            return source + speed;
+        }
+    }
+}
--- src/Assets/Scripts/Player/PlayerMovement.cs
+++ src/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,15 +1,107 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System;
+using System.Linq;
+using Assets.Scripts.Extensions;
+using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
+namespace Assets.Scripts.Player
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        public float MaxMovementSpeed;
+        public float MaxGravity;
+        public float JumpForce;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+        private bool _hasDoubleJumped;
+        private float _currentJumpForce;
+        private Rigidbody2D _rigidbody2D;
+
+        public float JumpDistance;
+
+        void Start()
+        {
+            _rigidbody2D = GetComponent<Rigidbody2D>();
+        }
+
+        void FixedUpdate()
+        {
+            var horizontalMovement = Input.GetAxisRaw("Horizontal") * MaxMovementSpeed;
+            if (Math.Abs(_currentJumpForce) > .1f)
+            {
+                horizontalMovement += _currentJumpForce;
+                _currentJumpForce = _currentJumpForce.MoveTowards(0, .3f);
+            }
+
+            if (IsGrounded())
+            {
+                _hasDoubleJumped = false;
+            }
+
+            _rigidbody2D.velocity = new Vector2(horizontalMovement, _rigidbody2D.velocity.y.Clamp(-MaxGravity, MaxGravity));
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if (GetRightHits())
+                {
+                    Jump(-JumpForce*.25f);
+                }
+
+                if (GetLeftHits())
+                {
+                    Jump(JumpForce*.25f);
+                }
+
+                if (IsGrounded())
+                {
+                    Jump();
+                }
+            }
+        }
+
+        private bool IsGrounded()
+        {
+            RaycastHit2D[] hits =
+            {
+                Physics2D.Raycast(transform.position + new Vector3(-.12f, -.4f), -Vector2.up, JumpDistance),
+                Physics2D.Raycast(transform.position + new Vector3(0, .4f), -Vector2.up, JumpDistance),
+                Physics2D.Raycast(transform.position + new Vector3(.12f, -.4f), -Vector2.up, JumpDistance)
+            };
+
+            return hits.Where(x => x.collider != null && x.collider.tag != "Player").Any();
+        }
+
+        private bool GetLeftHits()
+        {
+            RaycastHit2D[] hits =
+            {
+                Physics2D.Raycast(transform.position + new Vector3(-.4f, -.12f), Vector2.left, JumpDistance),
+                Physics2D.Raycast(transform.position + new Vector3(-.4f, 0), Vector2.left, JumpDistance),
+                Physics2D.Raycast(transform.position + new Vector3(-.4f, -.12f), Vector2.left, JumpDistance)
+            };
+
+            return hits.Where(x => x.collider != null && x.collider.tag != "Player").Any();
+        }
+
+        private bool GetRightHits()
+        {
+            RaycastHit2D[] hits =
+            {
+                Physics2D.Raycast(transform.position + new Vector3(.4f, -.12f), Vector2.right, JumpDistance),
+                Physics2D.Raycast(transform.position + new Vector3(.4f, 0), Vector2.right, JumpDistance),
+                Physics2D.Raycast(transform.position + new Vector3(.4f, -.12f), Vector2.right, JumpDistance)
+            };
+
+            return hits.Where(x => x.collider != null && x.collider.tag != "Player").Any();
+        }
+
+        private void Jump(float? horizontal = null)
+        {
+            _currentJumpForce = horizontal ?? 0;
+            transform.SetParent(null);
+            _rigidbody2D.velocity = new Vector2(horizontal ?? _rigidbody2D.velocity.x, JumpForce);
+            _rigidbody2D.gravityScale = 1;
+        }
+    }
 }
